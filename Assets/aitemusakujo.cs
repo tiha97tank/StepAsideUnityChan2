22c88feafbdee47c400d098978888dc3d76fab92
tiha97tank <tiha97tank@yahoo.co.jp>
2018-08-15 16:30:05 +0900

    StepAsideUnityChanの課題ファイルです。

---
+++ Assets/aitemusakujo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aitemusakujo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
